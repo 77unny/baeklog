76d1aab78dc7f8ffa380623c24679d9147fcf982
baekCode <baekcode@gmail.com>
2021-01-20 21:22:45 +0900

    feat : router

--- server/app.js
+++ server/app.js
@@ -1,17 +1,32 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cors from 'cors';
+import morgan from 'morgan';
 import config from './config';
+import postRouters from './routes/api/post';
+import userRouters from './routes/api/user';
 
 const app = express();
 const {MONGO_URI} = config;
 
+app.use(hpp());
+app.use(helmet());
+app.use(cors({origin: true, credential: true}));
+app.use(morgan('dev'));
+app.use(express.json());
+
 mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  useNewUrlParser   : true,
+  useUnifiedTopology: true,
+  useCreateIndex    : true
 })
   .then(() => console.log('[MongoDB connecting Success'))
   .catch(e => console.log(e));
 
 app.get('/');
+app.use('/api/post', postRouters);
+app.use('/api/user', userRouters);
 
 export default app;
