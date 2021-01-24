3aafcbc287913e9abdff1cfd95e9e4406acc0825
baekCode <baekcode@gmail.com>
2021-01-24 20:26:20 +0900

    feat : router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'connected-react-router';
+import store, {history} from './store';
+import Routers from './routes';
 
-function App(props) {
+function App() {
   return (
-    <div></div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Routers/>
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
---
+++ client/src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../conponents/Header';
+import Footer from '../conponents/Footer';
+
+function Routers(props) {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  );
+}
+
+export default Routers;
