372ac81b2f6aeeb531e80b48f76aad70bf171940
baekCode <baekcode@gmail.com>
2021-01-25 21:58:59 +0900

    modify : Router Layout

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '../conponents/Header';
 import Footer from '../conponents/Footer';
+import {Layout} from 'antd';
 
 function Routers(props) {
   return (
-    <>
+    <Layout>
       <Header/>
       <Footer/>
-    </>
+    </Layout>
   );
 }
 
