e5748f46adf0dc9d013d7e8f1205c8523acb02d6
baekCode <baekcode@gmail.com>
2021-01-25 21:57:45 +0900

    feat : styled

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import {ThemeProvider} from 'styled-components';
 import {ConnectedRouter} from 'connected-react-router';
 import store, {history} from './store';
 import Routers from './routes';
+import theme from './styles/Theme';
+import GlobalStyle from './styles/Global';
 
 function App() {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
         <Routers/>
+        </ThemeProvider>
       </ConnectedRouter>
     </Provider>
   );
