058403b556a554253eb2fa38d866109d9a1db58d
baekCode <baekcode@gmail.com>
2021-01-25 21:57:16 +0900

    feat : style theme

---
+++ client/src/styles/Theme.js
@@ -0,0 +1,18 @@
+const color = {
+  default: '#333',
+  white  : '#fff',
+  gray   : '#999',
+};
+
+const backgroundColor = {
+  black      : '#333',
+  gray       : '#f8f8f8',
+  transparent: 'transparent',
+};
+
+const theme = {
+  color,
+  backgroundColor
+};
+
+export default theme;
