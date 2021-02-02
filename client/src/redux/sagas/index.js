9443ff349da64278d1eb37de68435a5b6d71f53d
baekCode <baekcode@gmail.com>
2021-02-02 23:22:39 +0900

    feat : connect post (redux)

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 import {all, fork} from 'redux-saga/effects';
 import dotenv from 'dotenv';
 import authSaga from './authSaga';
+import postSaga from './postSaga';
 
 dotenv.config();
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
 
 export default function* rootSaga() {
-  yield all([fork(authSaga)]);
+  yield all([fork(authSaga), fork(postSaga)]);
 }
