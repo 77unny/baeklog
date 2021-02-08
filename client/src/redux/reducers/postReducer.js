19896a88585569b853e39997b5a264d71418e4aa
baekCode <baekcode@gmail.com>
2021-02-08 23:39:25 +0900

    modify : postReducer request initial posts(list)

--- client/src/redux/reducers/postReducer.js
+++ client/src/redux/reducers/postReducer.js
@@ -19,6 +19,7 @@ export default function (state = initialState, action) {
     case POSTS_LOADING_REQUEST :
       return {
         ...state,
+        posts  : [],
         loading: true,
       };
     case POSTS_LOADING_SUCCESS :
