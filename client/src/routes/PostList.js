1f6ebf564314833039ce43ce7055562ce0ca45e8
baekCode <baekcode@gmail.com>
2021-02-10 11:01:37 +0900

    feat : antd List style

--- client/src/routes/PostList.js
+++ client/src/routes/PostList.js
@@ -1,8 +1,10 @@
 import React, {useEffect} from 'react';
+import {Helmet} from 'react-helmet';
+import {Link} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {POSTS_LOADING_REQUEST} from '../redux/types';
-import {Helmet} from 'react-helmet';
-import {Spin} from 'antd';
+import {List, Space, Spin} from 'antd';
+import {MessageOutlined} from '@ant-design/icons';
 import PostCard from '../conponents/PostCard';
 
 function PostList(props) {
@@ -16,8 +18,36 @@ function PostList(props) {
   return (
     <>
       <Helmet title="POSTS"/>
-      {posts ? posts.map(item => <PostCard key={item._id} _id={item._id} title={item.title} fileUrl={item.fileUrl}
-                                           comments={item.comments} views={item.views}/>) : <Spin/>}
+      {posts ?
+        <List
+          itemLayout="vertical"
+          pagination={{
+            onChange: page => {
+              console.log(page);
+            },
+            pageSize: 5,
+          }}
+          dataSource={posts}
+          renderItem={post => {
+            return <List.Item
+              key={post._id}
+              actions={[<Space><MessageOutlined/>{post.comments.length}</Space>]}
+              extra={
+                <img
+                  width={272}
+                  alt={post.title}
+                  src={post.fileUrl}
+                />
+              }>
+              <List.Item.Meta
+                title={<Link to={`/posts/${post._di}`}>{post.title}</Link>}
+                description={post.description}
+              />
+              {post.content}
+            </List.Item>;
+          }}/>
+        : <Spin/>
+      }
     </>
   );
 }
