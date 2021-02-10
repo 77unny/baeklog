6959ea8679cb7c0ebdad242c7929f94baea9f199
baekCode <baekcode@gmail.com>
2021-02-10 11:18:31 +0900

    fix : same name components error

---
+++ client/src/conponents/Posts/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {List, Space} from 'antd';
+import {MessageOutlined} from '@ant-design/icons';
+
+function Posts({posts}) {
+  return (
+    <List
+      itemLayout="vertical"
+      pagination={{
+        onChange: page => {
+          console.log(page);
+        },
+        pageSize: 5,
+      }}
+      dataSource={posts}
+      renderItem={post => {
+        return <List.Item
+          key={post._id}
+          actions={[<Space><MessageOutlined/>{post.comments.length}</Space>]}
+          extra={
+            <img
+              width={272}
+              alt={post.title}
+              src={post.fileUrl}
+            />
+          }>
+          <List.Item.Meta
+            title={<Link to={`/posts/${post._id}`}>{post.title}</Link>}
+            description={post.description}
+          />
+          {post.content}
+        </List.Item>;
+      }}/>
+  );
+}
+
+export default Posts;
--- client/src/routes/PostList.js
+++ client/src/routes/PostList.js
@@ -1,11 +1,9 @@
 import React, {useEffect} from 'react';
 import {Helmet} from 'react-helmet';
-import {Link} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {POSTS_LOADING_REQUEST} from '../redux/types';
-import {List, Space, Spin} from 'antd';
-import {MessageOutlined} from '@ant-design/icons';
-import PostCard from '../conponents/PostCard';
+import {Spin} from 'antd';
+import Posts from '../conponents/Posts';
 
 function PostList(props) {
   const {posts} = useSelector(state => state.posts);
@@ -18,35 +16,7 @@ function PostList(props) {
   return (
     <>
       <Helmet title="POSTS"/>
-      {posts ?
-        <List
-          itemLayout="vertical"
-          pagination={{
-            onChange: page => {
-              console.log(page);
-            },
-            pageSize: 5,
-          }}
-          dataSource={posts}
-          renderItem={post => {
-            return <List.Item
-              key={post._id}
-              actions={[<Space><MessageOutlined/>{post.comments.length}</Space>]}
-              extra={
-                <img
-                  width={272}
-                  alt={post.title}
-                  src={post.fileUrl}
-                />
-              }>
-              <List.Item.Meta
-                title={<Link to={`/posts/${post._di}`}>{post.title}</Link>}
-                description={post.description}
-              />
-              {post.content}
-            </List.Item>;
-          }}/>
-        : <Spin/>
+      {posts ? <Posts posts={posts}/> : <Spin/>
       }
     </>
   );
