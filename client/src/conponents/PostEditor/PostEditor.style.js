a01e57955480caf94a1056da6537552a7367a058
baekCode <44539224+baekCode@users.noreply.github.com>
2021-02-13 12:57:53 +0900

    feat : editor (ckeditor)

---
+++ client/src/conponents/PostEditor/PostEditor.style.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import {Form, Input, Button} from 'antd';
+
+export const Container = styled.div``;
--- client/src/conponents/PostEditor/index.js
+++ client/src/conponents/PostEditor/index.js
@@ -1,34 +1,68 @@
 import React from 'react';
 import {CKEditor} from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import dotenv from 'dotenv';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import {editorConfiguration} from './EditorConfig';
+import MyCustomInit from './UploadAdapter';
+import {Container} from '../Footer/Footer.style';
+import {Button, Form, Input} from 'antd';
 
-dotenv.config();
 
-function PostEditor(props) {
+const Editor = () => {
+  const onFinish = (value) => console.log(value);
+  const onFinishFailed = (err) => console.log(err);
+
   return (
-    <div>
-      <CKEditor
-        editor={ClassicEditor}
-        data="<p>Hello from CKEditor 5!</p>"
-        onReady={editor => {
-          // You can store the "editor" and use when it is needed.
-          console.log('Editor is ready to use!', editor);
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          console.log({event, editor, data});
-        }}
-        onBlur={(event, editor) => {
-          console.log('Blur.', editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log('Focus.', editor);
-        }}
-      />
-
-    </div>
+    <Container>
+      <Form
+        name="editor"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="title"
+          name="title"
+          rules={[{required: true, message: '제목을 입력해주세요.'}]}
+        >
+          <Input/>
+        </Form.Item>
+        <Form.Item
+          label="category"
+          name="category"
+          rules={[{required: true, message: '카테고리를 입력해주세요.'}]}
+        >
+          <Input/>
+        </Form.Item>
+        <Form.Item
+          label="contents"
+          name="contents"
+          getValueFromEvent={(event, editor) => {
+            const data = editor.getData();
+            return data;
+          }}
+          rules={[{required: true, message: '내용을 입력해주세요.'}]}
+        >
+          <CKEditor
+            editor={ClassicEditor}
+            config={editorConfiguration}
+            onReady={MyCustomInit}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              console.log({event, editor, data});
+            }}
+            onBlur={(event, editor) => {
+              console.log('Blur.', editor);
+            }}
+            onFocus={(event, editor) => {
+              console.log('Focus.', editor);
+            }}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">Submit</Button>
+        </Form.Item>
+      </Form>
+    </Container>
   );
-}
+};
 
-export default PostEditor;
+export default Editor;
