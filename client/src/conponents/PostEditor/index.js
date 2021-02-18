4a06d99fdbdc23c635ff77bb69b4eb500ac53c53
baekCode <44539224+baekCode@users.noreply.github.com>
2021-02-18 23:30:12 +0900

    modify : ckeditor, thumnail image

--- client/src/conponents/PostEditor/index.js
+++ client/src/conponents/PostEditor/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import dotenv from 'dotenv';
 import {CKEditor} from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import {editorConfiguration} from './EditorConfig';
@@ -6,11 +7,12 @@ import MyCustomInit from './UploadAdapter';
 import {Container} from '../Footer/Footer.style';
 import {Button, Form, Input} from 'antd';
 
+dotenv.config();
 
 const Editor = () => {
+  const [thumbnail, setThumbnail] = useState(null);
   const onFinish = (value) => console.log(value);
   const onFinishFailed = (err) => console.log(err);
-
   return (
     <Container>
       <Form
@@ -45,19 +47,45 @@ const Editor = () => {
             editor={ClassicEditor}
             config={editorConfiguration}
             onReady={MyCustomInit}
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              console.log({event, editor, data});
-            }}
             onBlur={(event, editor) => {
-              console.log('Blur.', editor);
+              const data = editor.getData();
+
+              if (data && data.match('<img src=')) {
+                console.log('[data]', data);
+                const extensionType = ['jpg', 'jpeg', 'png', 'gif'];
+                const imageStartPoint = data.indexOf('<img src=');
+                let imageEndPoint;
+                let findExtensionName;
+                let resultImageUrl;
+
+                extensionType.forEach((item, i) => {
+                  if (data.match(item)) {
+                    console.log('indexOfData', data.indexOf(item));
+                    findExtensionName = item;
+                    imageEndPoint = data.indexOf(item);
+                  }
+                });
+
+                if (findExtensionName === 'jpeg') {
+                  resultImageUrl = data.substring(imageStartPoint + 10, imageEndPoint + 4);
+                } else {
+                  resultImageUrl = data.substring(imageStartPoint + 10, imageEndPoint + 3);
+                }
+
+                setThumbnail(resultImageUrl);
+
+              } else {
+                setThumbnail(process.env.REACT_APP_DEFAULT_THUMBNAIL);
+              }
+
             }}
-            onFocus={(event, editor) => {
-              console.log('Focus.', editor);
-            }}
+
+            // onFocus={(event, editor) => {
+            //   console.log('Focus.', editor);
+            // }}
           />
         </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Form.Item wrapperCol={{offset: 8, span: 16}}>
           <Button type="primary" htmlType="submit">Submit</Button>
         </Form.Item>
       </Form>
