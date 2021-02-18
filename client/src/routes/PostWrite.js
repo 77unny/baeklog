27a53167bf1520048b2d82e1dfa3df5af6eb8e03
baekCode <44539224+baekCode@users.noreply.github.com>
2021-02-18 23:29:31 +0900

    modify : post write router, add isAuth

--- client/src/routes/PostWrite.js
+++ client/src/routes/PostWrite.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PostEditor from '../conponents/PostEditor';
+import {useSelector} from 'react-redux';
 
 function PostWrite(props) {
+  const {isAuthenticated} = useSelector(state => state.auth);
   return (
-    <div>PostWrite
-      <PostEditor/>
+    <div>
+      {isAuthenticated && <PostEditor/>}
     </div>
   );
 }
