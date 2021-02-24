2db7d835ba1906c4aa5ca95ee47a4579d53692d4
baekCode <44539224+baekCode@users.noreply.github.com>
2021-02-24 23:28:51 +0900

    feat : redux, post reducer & post saga (upload,detail)

--- client/src/redux/reducers/postReducer.js
+++ client/src/redux/reducers/postReducer.js
@@ -1,4 +1,14 @@
-import {POSTS_LOADING_FAILURE, POSTS_LOADING_REQUEST, POSTS_LOADING_SUCCESS} from '../types';
+import {
+  POST_DETAIL_FAILURE,
+  POST_DETAIL_REQUEST,
+  POST_DETAIL_SUCCESS,
+  POSTS_LOADING_FAILURE,
+  POSTS_LOADING_REQUEST,
+  POSTS_LOADING_SUCCESS,
+  POSTS_UPLOAD_FAILURE,
+  POSTS_UPLOAD_REQUEST,
+  POSTS_UPLOAD_SUCCESS
+} from '../types';
 
 const initialState = {
   isAuthenticated   : null,
@@ -31,6 +41,44 @@ export default function (state = initialState, action) {
     case POSTS_LOADING_FAILURE :
       return {
         ...state,
+        loading: false,
+      };
+    case POST_DETAIL_REQUEST :
+      return {
+        ...state,
+        posts  : [],
+        loading: true,
+      };
+    case POST_DETAIL_SUCCESS :
+      return {
+        ...state,
+        postDetail: action.payload,
+        createId  : action.payload.creator._id,
+        title     : action.payload.title,
+        loading   : false,
+      };
+    case POST_DETAIL_FAILURE :
+      return {
+        ...state,
+        error  : action.payload,
+        loading: false,
+      };
+    case POSTS_UPLOAD_REQUEST :
+      return {
+        ...state,
+        loading: true,
+      };
+    case POSTS_UPLOAD_SUCCESS :
+      return {
+        ...state,
+        posts          : action.payload,
+        isAuthenticated: true,
+        loading        : false,
+      };
+    case POSTS_UPLOAD_FAILURE :
+      return {
+        ...state,
+        error  : action.payload,
         loading: false,
       };
     default :
--- client/src/redux/sagas/postSaga.js
+++ client/src/redux/sagas/postSaga.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 import {all, call, fork, put, takeEvery} from 'redux-saga/effects';
 import {push} from 'connected-react-router';
 import {
+  POST_DETAIL_FAILURE,
+  POST_DETAIL_REQUEST,
+  POST_DETAIL_SUCCESS,
   POSTS_LOADING_FAILURE,
   POSTS_LOADING_REQUEST,
   POSTS_LOADING_SUCCESS,
@@ -17,8 +20,6 @@ const loadPostAPI = () => {
 function* loadPosts() {
   try {
     const result = yield call(loadPostAPI);
-    console.log('[loadPostsAPI]', result);
-
     yield put({
       type   : POSTS_LOADING_SUCCESS,
       payload: result.data
@@ -42,28 +43,25 @@ const uploadPostAPI = (payload) => {
       'Content-Type': 'application/json'
     }
   };
-  const token = payload.token
+  const token = payload.token;
   if (token) config.headers['x-auth-token'] = token;
   return axios.post('/api/post', payload, config);
 };
 
 function* uploadPosts(action) {
   try {
-    console.log('[uploadPostsAPI action]', action)
     const result = yield call(uploadPostAPI, action.payload);
-    console.log('[uploadPostsAPI]', result);
-
     yield put({
       type   : POSTS_UPLOAD_SUCCESS,
       payload: result.data
     });
-    yield put(push(`/post/${result.data._id}`))
+    yield put(push(`/post/${result.data._id}`));
   } catch (e) {
     yield put({
       type   : POSTS_UPLOAD_FAILURE,
       payload: e
     });
-    yield push('/');
+    yield put(push('/'));
   }
 }
 
@@ -71,6 +69,36 @@ function* watchUploadPosts() {
   yield takeEvery(POSTS_UPLOAD_REQUEST, uploadPosts);
 }
 
+const postDetailAPI = (id) => {
+  return axios(`/api/post/${id}`);
+};
+
+function* postDetail(action) {
+  try {
+    const result = yield call(postDetailAPI, action.payload);
+    console.log('[postDetailAPI 2]', result);
+
+    yield put({
+      type   : POST_DETAIL_SUCCESS,
+      payload: result.data
+    });
+  } catch (e) {
+    yield put({
+      type   : POST_DETAIL_FAILURE,
+      payload: e
+    });
+    yield put(push('/'));
+  }
+}
+
+function* watchPostDetail() {
+  yield takeEvery(POST_DETAIL_REQUEST, postDetail);
+}
+
 export default function* postSaga() {
-  yield all([fork(watchLoadPosts), fork(watchUploadPosts)]);
+  yield all([
+    fork(watchLoadPosts),
+    fork(watchUploadPosts),
+    fork(watchPostDetail)
+  ]);
 }
